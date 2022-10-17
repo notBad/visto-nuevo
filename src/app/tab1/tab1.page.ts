12c6fe341a76c9a179512d762fb31febf875b183
JoseL <firing4@gmail.com>
2022-10-17 21:58:02 +0200

    cambio botones supoeriores

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { flushMicrotasks } from '@angular/core/testing';
 
 @Component({
   selector: 'app-tab1',
@@ -7,7 +8,15 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  dia = false;
+  semana = false;
+  mes = false;
+
+  constructor() {
+
+  }
+
+
 
   click(){
 /*     alert('hola'); */
@@ -84,9 +93,7 @@ export class Tab1Page {
     //Append the table header and all records
     const table = document.getElementById('listado');
     table.innerHTML = headerRowHTML + allRecordsHTML;
-    //Aplicar CSS
-    const test = document.getElementById('testeo2');
-    test.style.backgroundColor = '#FF00FF';
+
 
     //this.refreshCSS();
   }
@@ -106,5 +113,60 @@ export class Tab1Page {
         }
     }
   };
+
+  clickDia(){
+    //Aplicar CSS
+    const frontal = document.getElementById('dia');
+    const fondo = document.getElementById('dia_fondo');
+
+    if (!this.dia) {
+     frontal.style.backgroundColor = '#28ba62';
+     frontal.style.transform = 'translateY(-4px)';
+      fondo.style.backgroundColor = '#0D6E34';
+      this.dia = true;
+    } else {
+     frontal.style.backgroundColor = '#787878';
+     fondo.style.backgroundColor = '#545454';
+     frontal.style.transform = 'translateY(-8px)';
+      this.dia = false;
+    }
+
+  }
+  clickSemana(){
+    //Aplicar CSS
+    const frontal = document.getElementById('semana');
+    const fondo = document.getElementById('semana_fondo');
+
+    if (!this.semana) {
+     frontal.style.backgroundColor = '#28ba62';
+     frontal.style.transform = 'translateY(-4px)';
+      fondo.style.backgroundColor = '#0D6E34';
+      this.semana = true;
+    } else {
+     frontal.style.backgroundColor = '#787878';
+     fondo.style.backgroundColor = '#545454';
+     frontal.style.transform = 'translateY(-8px)';
+      this.semana = false;
+    }
+
+  }
+  clickMes(){
+    //Aplicar CSS
+    const frontal = document.getElementById('mes');
+    const fondo = document.getElementById('mes_fondo');
+
+    if (!this.mes) {
+     frontal.style.backgroundColor = '#28ba62';
+     frontal.style.transform = 'translateY(-4px)';
+      fondo.style.backgroundColor = '#0D6E34';
+      this.mes = true;
+    } else {
+     frontal.style.backgroundColor = '#787878';
+     fondo.style.backgroundColor = '#545454';
+     frontal.style.transform = 'translateY(-8px)';
+      this.mes = false;
+    }
+
+  }
 }
 
